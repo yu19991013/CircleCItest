ecce5813621989457ca968b871d046176f418abc
yu19991013 <y19981013@icloud.com>
2018-11-27 21:31:25 +0900

    config.yamlの環境変数を変更

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | sudo tee /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
 ./ecs-deploy --cluster circleci-go-test --service-name circleci-go-test --image $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$ECR_REPOSITORY_NAME:$CIRCLE_SHA1
